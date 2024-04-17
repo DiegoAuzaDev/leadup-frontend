d26563ed2416c60406eb5782e64a0f6efad9e722
auza0002 <auzajuan1@gmail.com>
2024-04-17 13:15:57 -0400

    Adding loading indicator and error hanlde on sign in

--- src/utils/localAuth.jsx
+++ src/utils/localAuth.jsx
@@ -1,26 +1,41 @@
-import { API_URL, DASHBOARD_URL} from "./keys.jsx"
+import { API_URL, DASHBOARD_URL } from "./keys.jsx";
 
-const localSignup = async (name , email, password) => {
-
-    const reqBody = {
-      "name" : name,
-      "email" : email,
-      "password" : password
-    };
-    const localSignUpRequest = await fetch(
-      `${API_URL}/auth/signup?redirect_url=${DASHBOARD_URL}`,
-      {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(reqBody),
-      }
-    );
-    return localSignUpRequest
-
+const localSignup = async (name, email, password) => {
+  const reqBody = {
+    name: name,
+    email: email,
+    password: password,
+  };
+  const localSignUpRequest = await fetch(
+    `${API_URL}/auth/signup?redirect_url=${DASHBOARD_URL}`,
+    {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqBody),
+    }
+  );
+  return localSignUpRequest;
 };
-const localSignin = (email, password) => {};
+const localSignin = async (email, password) => {
+  const reqBody = {
+    "email": email,
+    "password": password,
+  };
+   const localSignInRequest = await fetch(
+     `${API_URL}/auth/login?redirect_url=${DASHBOARD_URL}`,
+     {
+       method: "POST",
+       mode: "cors",
+       headers: {
+         "Content-Type": "application/json",
+       },
+       body: JSON.stringify(reqBody),
+     }
+   );
+   return localSignInRequest;
+};
 
 export { localSignup, localSignin };
