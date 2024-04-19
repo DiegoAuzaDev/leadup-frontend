88d59030d2f055888580ab20c63b3aba13bcd2ea
auza0002 <auzajuan1@gmail.com>
2024-04-19 12:33:50 -0400

    adding loading indicator

--- src/pages/workspace/workspace.jsx
+++ src/pages/workspace/workspace.jsx
@@ -13,11 +13,12 @@ function Workspace() {
   const urlToken = searchParams.get("token");
 
   // user data
+  const [isLoading, setIsLoading] = useState(true);
   const [userName, setUserName] = useState("");
   const [userEmail, setUserEmail] = useState("");
   const [userPhoto, setUserPhoto] = useState("");
-  const [error, setError] = useState(null)
-  const [errorMessage, setErrorMessage] = useState("")
+  const [error, setError] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     document.body.classList.remove("bg-primary");
@@ -40,36 +41,40 @@ function Workspace() {
   }, [token]);
 
   const getUserData = async () => {
-    setError(null)
-    setErrorMessage("")
-    
+    setError(null);
+    setErrorMessage("");
     try {
       const response = await requestuserData(token);
       if (response.ok && response.status == 200) {
+        setIsLoading(false);
         console.log(response);
         const body = await response.json();
         console.log(body);
       }
-      if(!response.ok && response.status == 401){
+      if (!response.ok && response.status == 401) {
         throw new Error("Unauthorized, Error status : 401");
       }
-      if(!response.ok){
-         throw new Error("Server Error, Error status : 500");
+      if (!response.ok) {
+        throw new Error("Server Error, Error status : 500");
       }
     } catch (err) {
-      setError(true)
-      setErrorMessage(err.message)
+      // setIsLoading(false);
+      // setError(true);
+      setErrorMessage(err.message);
     }
   };
 
   return (
     <div className="grid grid-cols-12  px-3 py-5 md:px-0 md:py-0 grid-rows-1 gap-0 md:grid-cols-12 md:grid-rows-2 md:gap-x-3 md:gap-y-5 h-[100vh] w-[100vw]">
       <HeaderWorkSpace />
+      {isLoading && (
+        <>
+          <h1>Is Loading</h1>
+        </>
+      )}
       {error && <ErrorWorkspace message={errorMessage} />}
     </div>
   );
 }
-
-
 
 export default Workspace;
