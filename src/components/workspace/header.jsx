c1313db3505a611c38ea7b55bbe3e6e584c4b707
Diego Auza <diegoauza@Diegos-MacBook-Pro.local>
2024-04-29 07:58:03 -0500

    Header mobile styles added

--- src/components/workspace/header.jsx
+++ src/components/workspace/header.jsx
@@ -21,7 +21,7 @@ function HeaderWorkSpace({ user }) {
   return (
     <header
       style={{ width: "-webkit-fill-available" }}
-      className=" sm:absolute sm:mr-3 sm:col-span-12 sm:rounded-custom md:m-0 md:rounded-none px-3 py-5  md:relative md:grid md:px-3 md:py-5 md:row-span-2 md:col-span-2 md:border-2 md:border-y-0 md:border-r-surface-dark md:border-l-0 bg-primary text-white"
+      className=" absolute mr-3 sm:col-span-12 rounded-custom md:m-0 md:rounded-none px-3 py-2  md:relative md:grid md:px-3 md:py-5 md:row-span-2 md:col-span-2 md:border-2 md:border-y-0 md:border-r-surface-dark md:border-l-0 bg-primary text-white"
     >
       <nav className=" hidden md:flex flex-col justify-between">
         <ul
@@ -107,24 +107,76 @@ function HeaderWorkSpace({ user }) {
         </ul>
       </nav>
       <nav className=" md:hidden">
-        <ul role="list" className=" flex flex-row justify-between">
+        <ul role="list" className=" flex flex-row justify-between items-center">
           <li>
             <NavLink className="">
-              <img className=" h-10 " src={LeadUpIconWhite} alt="LeadUp Logo" />
+              <img
+                className=" h-[1.5rem] "
+                src={LeadUpIconWhite}
+                alt="LeadUp Logo"
+              />
             </NavLink>
           </li>
           <li>
             <button
+              className=" flex"
               onClick={(ev) => {
                 ev.preventDefault();
                 setIsActive(!isActive);
               }}
             >
               <FontAwesomeIcon
-                icon={isActive ? faBars : faClose}
-                className=" text-[2.5rem]"
+                icon={!isActive ? faBars : faClose}
+                className=" text-[1.5rem]"
               />
             </button>
+          </li>
+        </ul>
+        <ul
+          role="list"
+          className={` absolute bg-primary right-0 rounded-custom  top-[3rem] z-50  overflow-hidden flex flex-col gap-2 ${
+            isActive ? " p-2 h-auto" : "h-0"
+          } `}
+        >
+          <li>
+            <NavLink className="navlink" to="/leadUp/workspace/dashboard">
+              <div>
+                <FontAwesomeIcon icon={faTableList} />
+                <p className=" m-0">Dashboard</p>
+              </div>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navlink" to="/leadUp/workspace/calendar">
+              <div>
+                <FontAwesomeIcon icon={faCalendarDays} />
+                <p className=" m-0">Calendar</p>
+              </div>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navlink" to="/leadUp/workspace/team">
+              <div>
+                <FontAwesomeIcon icon={faPeopleGroup} />
+                <p className=" m-0">Team</p>
+              </div>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navlink" to="/leadUp/workspace/vehicles">
+              <div>
+                <FontAwesomeIcon icon={faTruckFront} />
+                <p className=" m-0">Vehicles</p>
+              </div>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="navlink" to="/leadUp/workspace/settings">
+              <div>
+                <FontAwesomeIcon icon={faGear} />
+                <p className=" m-0">Settings</p>
+              </div>
+            </NavLink>
           </li>
         </ul>
       </nav>
